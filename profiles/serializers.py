43058e3c13300d5924edb7942a7ec2a41a2c747b
Igor Basuga <igor.basuga@gmail.com>
2021-09-19 10:14:32 +0000

    add the posts app; create Post model; complete PostSerializer challenge

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -15,5 +15,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             "id", "owner",
             "created_at", "updated_at",
-            "name", "content", "image", "is_owner"
+            "name", "content", "image"
         ]
