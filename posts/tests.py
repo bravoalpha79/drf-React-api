eef848f002e60874aac182f928cf3174f1512fe1
Igor Basuga <igor.basuga@gmail.com>
2021-09-21 13:53:30 +0000

    add test cases to list and add posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username="igor", password="password123")
+
+    def test_can_list_posts(self):
+        igor = User.objects.get(username="igor")
+        Post.objects.create(owner=igor, title="a title")
+        response = self.client.get("/posts/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username="igor", password="password123")
+        count = Post.objects.count()
+        self.assertEqual(count, 0)
+        response = self.client.post("/posts/", {"title": "a title"})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_out_user_cannot_create_post(self):
+        count = Post.objects.count()
+        self.assertEqual(count, 0)
+        response = self.client.post("/posts/", {"title": "a title"})
+        count = Post.objects.count()
+        self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
