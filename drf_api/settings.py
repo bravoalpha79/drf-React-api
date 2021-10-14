c975f4584c64d1f94fda60caf3efa8d51b7ff788
Igor Basuga <igor.basuga@gmail.com>
2021-10-14 16:44:15 +0000

    fix typo in database settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -150,7 +150,8 @@ DATABASES = {
     'default': ({
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    } if "DEV" in os.environ else dj_database_url.parse("DATABASE_URL"))
+    } if "DEV" in os.environ else dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    )
 }
 
 
